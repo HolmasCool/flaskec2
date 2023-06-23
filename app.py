7628dbaab4d0f91b0290e419640de8bfb6c6985f
HolmasCool <wh.koo@epointpos.com>
2023-06-23 16:47:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ class epoint_data(db.Model):
 @app.route('/')
 
 def index():
-    return render_template('index2.html')
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
