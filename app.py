729f2951744f912d461750c112a96988acaf098c
HolmasCool <wh.koo@epointpos.com>
2023-07-06 17:01:40 +0800

    update

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from llama_index import SimpleDirectoryReader, GPTListIndex, readers, GPTSimpleVectorIndex, LLMPredictor,\
-    PromptHelper, ServiceContext
+from llama_index import SimpleDirectoryReader, GPTSimpleVectorIndex, LLMPredictor, PromptHelper, ServiceContext
+# , GPTListIndex, readers
 from langchain import OpenAI
-from pathlib import Path
+# from pathlib import Path
 from dotenv import load_dotenv
 import os
 # import sys
@@ -21,30 +21,25 @@ load_dotenv()
 app = Flask(__name__)
 
 app.config[
-    'SQLALCHEMY_DATABASE_URI'] = 'mysql://'+os.getenv('SQLUSERNAME')+':'+os.getenv('SQLPASSWORD')+'@'+\
-                                 os.getenv('SQLSERVER')+'.cb1yssr93nnk.ap-southeast-1.rds' \
-                                 '.amazonaws.com/EpointContext'
-
-# app.config[
-#     'SQLALCHEMY_DATABASE_URI'] = 'mysql://epointadmin:200200170hH@epointaichat.cb1yssr93nnk.ap-southeast-1.rds' \
-#                                  '.amazonaws.com/EpointContext'
-
-
+    'SQLALCHEMY_DATABASE_URI'] = 'mysql://' + os.getenv('SQLUSERNAME') + ':' + os.getenv('SQLPASSWORD') + '@' \
+                                 + os.getenv('SQLSERVER') + '.cb1yssr93nnk.ap-southeast-1.rds' \
+                                                            '.amazonaws.com/EpointContext'
 
 app.config['SQLACHEMY_TRACK_MODIFICATION'] = os.getenv("SQLACHEMY_TRACK_MODIFICATION")
 db = SQLAlchemy(app)
+
 
 # app.secret_key = "somethingsunique"
 
 class EpointData(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    question = db.Column(LONGTEXT, nullable=False)
     department = db.Column(db.String(100), nullable=False)
+    question = db.Column(LONGTEXT, nullable=False)
     answer = db.Column(LONGTEXT, nullable=False)
     if answer is None:
         answer = ''
     if department is None:
-        department = ''
+        department = 'Question'
 
     def __init__(self, question='', answer='', department=''):
         self.question = question
@@ -53,38 +48,76 @@ class EpointData(db.Model):
 
 
 @app.route('/')
-def index():
+def index(openaianswer='', openaiquestion='', epoint_datas=''):
+    openaianswer = request.args.get('openaianswer')
+    openaiquestion = request.args.get('openaiquestion')
+    epoint_datas = EpointData.query.filter(EpointData.department != 'Question').all()
+    return render_template('index.html', openaianswer=openaianswer, openaiquestion=openaiquestion, epoint_datas=epoint_datas)
+
+
+@app.route('/indexadmin/', methods=['GET'])
+def indexadmin():
     epoint_datas = EpointData.query.all()
-    return render_template('index.html', epoint_datas=epoint_datas)
+    return render_template('indexadmin.html', epoint_datas=epoint_datas, indicator='Admin')
+
+
+@app.route('/Delete/', methods=['GET'])
+def delete_epointdata_indexadmin():
+    db.session.delete(EpointData.query.get(request.args.get('id')))
+    db.session.commit()
+    return redirect(url_for('indexadmin'))
+
+@app.route('/Update/', methods=['GET'])
+def update_context_data():
+    file_to_use = open('context_data/data/epoint_data.txt', 'w+')
+    epoint_datas = EpointData.query.filter(EpointData.department != 'Question').all()
+
+    for epoint_data in epoint_datas :
+        file_to_use.write('Interviewer:'+epoint_data.question.strip() + '\n')
+        file_to_use.write('Interviewee:' +epoint_data.answer.strip() + '\n')
+    file_to_use.close()
+
+    return redirect(url_for('indexadmin'))
+
 
 
 @app.route('/add/', methods=['POST'])
-def send_question():
+def insert_question():
+    openaianswer = ''
+    openaiquestion = ''
+    department = ''
+    question = ''
+    answer = ''
+
     if request.method == "POST":
+        department = request.form.get('txtDepartment')
         question = request.form.get('question')
         answer = request.form.get('answer')
-        department = request.form.get('Question')
         if answer is None:
             answer = ''
         if department is None:
-            department = ''
+            department = 'Question'
+            answer = ''
 
-        openaianswer = ask_ai(question)
+        if answer == '' and department == 'Question':
+            openaianswer = ask_ai(question)
+            openaiquestion = question
 
     epointData = EpointData(question, answer, department)
     db.session.add(epointData)
     db.session.commit()
+    epoint_datas = EpointData.query.filter_by(department='Admin').all()
+
     #    flask("Book added successful")
-    #    return redirect(url_for('index'), openaianswer=openaianswer)
-    return render_template('index.html', openaianswer=openaianswer)
 
+    if request.form.get('isAdmin') == 'Admin':
+        return redirect(url_for('indexadmin', epoint_datas=epoint_datas))
+    else:
+        return redirect(
+            url_for('index', openaianswer=openaianswer, openaiquestion=openaiquestion, epoint_datas=epoint_datas))
 
-@app.route('/delete/', methods=['GET'])
-def delete_epointdata():
-    db.session.delete(EpointData.query.get(request.args.get('id')))
-    db.session.commit()
-    return redirect(url_for('index'))
-
+    # return render_template('index.html', openaianswer=openaianswer, openaiquestion=openaiquestion,
+    #                        epoint_datas=epoint_datas)
 
 
 def construct_index(directory_path):
@@ -106,24 +139,17 @@ def construct_index(directory_path):
     documents = SimpleDirectoryReader(directory_path).load_data()
 
     service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, prompt_helper=prompt_helper)
-    index = GPTSimpleVectorIndex.from_documents(documents, service_context=service_context)
-
-    index.save_to_disk('index.json')
-
-    return index
+    GPTindex = GPTSimpleVectorIndex.from_documents(documents, service_context=service_context)
+    GPTindex.save_to_disk('index.json')
+    return GPTindex
 
 
 def ask_ai(question):
-    #secret_key = openai_secret_key_entry.get()
-    #os.environ["OPENAI_API_KEY"] = secret_key
     os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
     construct_index("context_data/data")
-
-    index = GPTSimpleVectorIndex.load_from_disk('index.json')
-
-    response = index.query(question)
+    GPTindex = GPTSimpleVectorIndex.load_from_disk('index.json')
+    response = GPTindex.query(question)
     return response
-
 
 
 if __name__ == "__main__":
